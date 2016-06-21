f22aaeff42fd78f83d3a08303aa2ef4ba2ab439d
samuelandkevin <YHIOS002@yhios002demacbook-pro.local>
2016-06-21 17:12:56 +0800

    Initial Commit

---
+++ NewVoucher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewVoucher
+//
+//  Created by YHIOS002 on 16/6/21.
+//  Copyright © 2016年 YHSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewVoucher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewVoucher
+//
+//  Created by YHIOS002 on 16/6/21.
+//  Copyright © 2016年 YHSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
